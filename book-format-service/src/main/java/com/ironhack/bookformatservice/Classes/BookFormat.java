52a9ee84bb76c33d7121e6afe9b9f3502b0791ec
la Fuente Maschi <s.la.fuente.maschi@accenture.com>
2022-07-04 16:19:10 +0200

    GET

--- book-format-service/src/main/java/com/ironhack/bookformatservice/Classes/BookFormat.java
+++ book-format-service/src/main/java/com/ironhack/bookformatservice/Classes/BookFormat.java
@@ -4,4 +4,16 @@ import com.ironhack.bookformatservice.Enum.Format;
 
 public class BookFormat {
     private Format format;
+
+    public BookFormat(Format format) {
+        this.format = format;
+    }
+
+    public Format getFormat() {
+        return format;
+    }
+
+    public void setFormat(Format format) {
+        this.format = format;
+    }
 }
