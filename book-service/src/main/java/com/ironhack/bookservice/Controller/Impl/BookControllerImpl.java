709767b744abaaf905e71df1779a9780cb6dab44
la Fuente Maschi <s.la.fuente.maschi@accenture.com>
2022-07-04 18:20:27 +0200

    imports

--- book-service/src/main/java/com/ironhack/bookservice/Controller/Impl/BookControllerImpl.java
+++ book-service/src/main/java/com/ironhack/bookservice/Controller/Impl/BookControllerImpl.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
 public class BookControllerImpl implements BookController {
